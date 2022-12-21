cc935290db23f1466d030f35c952a3bc8d36dccd
xhaocherry <cyhcherry@outlook.com>
2022-12-21 20:32:18 +0800

    1

---
+++ Study/指针数组函数案例.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+//冒泡排序,参数首地址，数组长度
+void bubbleSort(int * arr,int len){
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = 0; j < len - i - 1; j++){
+			if (arr[j] > arr[j + 1]){
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+void printArray(int* arr, int len) {
+	for (int i = 0; i < len; i++) {
+		cout << arr[i] << endl;
+	}
+}
+int main() 
+{
+	//1.先创建数组
+	int arr[10] = { 3,5,1,7,85,23,17,56,44,33 };
+	//数组长度
+	int len = sizeof(arr) / sizeof(arr[0]);
+	//2.创建函数，实现冒泡排序
+	bubbleSort(arr, len);
+	//3.打印排序后的数组
+	printArray(arr,len);
+}
---
+++ Study/结构体定义和使用.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+//创建学生数据类型
+//自定义数据类型，一些类型的集合
+//语法 struct 类型名称{成员列表}
+struct Student
+{
+	//成员列表
+	//姓名
+	string name;
+	//分数
+	int score;
+	int age;
+} s3;//顺便创建变量
+//2.通过学生类型创建具体学生
+//2.1 struct Student s1
+//2.2 struct student s2={...}
+//2.3 在定义结构体时顺便创建具体学生
+int main() {
+	struct Student s1;//struct创建可省略，定义不可以
+	s1.name = "zhangsan";
+	s1.age = 14;
+	s1.score = 99;
+	cout << s1.name << s1.age << s1.score << endl;
+	struct Student s2 = { "zhang",19,90 };
+	cout << s2.name << s2.age << s2.score << endl;
+	s3.name = "zhang1";
+	cout << s3.name << endl;
+}
