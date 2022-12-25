237e5c2cd25636f0ae8612246f5805eb625bdcd5
xhaocherry <cyhcherry@outlook.com>
2022-12-26 01:07:34 +0800

    1

---
+++ Study/构造和析构函数.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+//对象的初始化和清理
+//构造函数 进行初始化操作
+class Person
+{
+public:
+	//没有返回值，不用void 函数名和类名相同，可以有参、能重载
+	//创建对象的时候，构造函数会自动调用，而且只调用一次
+	Person()
+	{
+		cout << "person构造函数的调用" << endl;
+	}
+	//析构函数进行清理操作
+	//没有返回值不写void 函数名和类名相同再名称前加~
+	//析构函数不可以有参数，不可以重载
+	//对象在销毁前会自动调用析构函数，且仅调一次
+	~Person()
+	{
+		cout << "person析构函数的调用" << endl;
+	}
+};
+//构造和析构都是必须有实现，如果我们自己不提供，编译器会自动提供
+void test1()
+{
+	Person p;//在栈上的数据，test1执行结束后，释放这个对象
+}
+int main()
+{
+	test1();
+	//Person p;
+	system("pause");
+	return 0;
+}
---
+++ Study/点和圆关系.cpp
@@ -0,0 +1,85 @@
+﻿#include<iostream>
+using namespace std;
+//点和圆关系
+class Point
+{
+public:
+	void setX(int x)
+	{
+		m_X = x;
+	}
+	int getX()
+	{
+		return m_X;
+	}
+	void setY(int y)
+	{
+		m_Y = y;
+	}
+	int getY()
+	{
+		return m_Y;
+	}
+private:
+	int m_X;
+	int m_Y;
+};
+class Circle
+{
+public:
+	void setR(int r)
+	{
+		m_R = r;
+	}
+	int getR()
+	{
+		return m_R;
+	}
+	void setCenter(Point center)
+	{
+		m_Center = center;
+	}
+	Point getCenter()
+	{
+		return m_Center;
+	}
+private:
+	int m_R;
+	//在类中可以让另一个类作为本来中的成员
+	Point m_Center;
+};
+void isInCircle(Circle& c, Point& p)
+{
+	//计算两点之间距离的平方
+	int distance =
+		(c.getCenter().getX() - p.getX()) * (c.getCenter().getX() - p.getX()) +
+		(c.getCenter().getY() - p.getY()) * (c.getCenter().getY() - p.getY());
+	int rDistance = c.getR() * c.getR();
+	if (distance == rDistance)
+	{
+		cout << "在圆上" << endl;
+	}
+	else if (distance > rDistance)
+	{
+		cout << "在圆外" << endl;
+	}
+	else
+	{
+		cout << "在圆内" << endl;
+	}
+}
+int main()
+{
+	//创建圆
+	Circle c;
+	c.setR(10);
+	Point center;
+	center.setX(10);
+	center.setY(0);
+	c.setCenter(center);
+	//创建点
+	Point p;
+	p.setX(10);
+	p.setY(10);
+	isInCircle(c, p);
+}
