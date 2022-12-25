98f73184e0f505e8e905dacd7eb95c414be56603
xhaocherry <cyhcherry@outlook.com>
2022-12-25 15:14:37 +0800

    Create 封装案例.cpp

---
+++ Study/封装案例.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+class student
+{
+public:
+	string name;
+	int id;
+	void show()
+	{
+		cout << "姓名"<<name<<"学号"<<id<<endl;
+	}
+	void setname(string name1)
+	{
+		name = name1;
+	}
+};
+int main()
+{
+	student stu;
+	stu.id = 12;
+	stu.setname("zhang");
+	stu.show();
+}
