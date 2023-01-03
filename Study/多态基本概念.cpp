ec08d497510e844742b46831f022cf71629cc7cd
xhaocherry <cyhcherry@outlook.com>
2023-01-03 20:53:14 +0800

    1

---
+++ Study/多态基本概念.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+class Animal {
+public:
+	//虚函数
+	virtual void speak() {
+		cout << "动物说话" << endl;
+	}
+};
+class Cat :public Animal {
+public:
+	void speak() {
+		cout << "猫说话" << endl;
+	}
+};
+//地址早绑定 在编译阶段确认函数地址
+//如果想让猫先说话，那么这个函数地址就不能提前绑定
+void doSpeak(Animal& animal) {
+	animal.speak();
+}
+void test1() {
+	Cat cat;
+	doSpeak(cat);
+}
+int main() {
+	test1();
+}
---
+++ Study/菱形继承.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+//动物类
+class Animal {
+public:
+	int m_Age;
+};
+//利用虚继承解决菱形继承的问题
+//继承之前加上关键字virtual变为虚继承
+//羊类
+class Sheep :virtual public Animal {};
+//驼类
+class Tuo :virtual public Animal {};
+//羊驼类
+class SheepTuo :public Sheep, public Tuo {};
+void test1() {
+	SheepTuo st;
+	st.Sheep::m_Age = 18;
+	st.Tuo::m_Age = 28;
+	//当菱形继承，两个父类拥有相同数据，要加作用域区分
+	cout << "st.Sheep::m_Age=" << st.Sheep::m_Age << endl;
+	cout << "st.Tuo::m_Age=" << st.Tuo::m_Age << endl;
+}
+int main() {
+	test1();
+}
