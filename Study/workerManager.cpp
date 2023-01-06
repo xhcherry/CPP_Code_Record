284945938247ce0a88c360a1fe8fc27c978c51c7
xhaocherry <cyhcherry@outlook.com>
2023-01-06 22:23:54 +0800

    1

--- Study/workerManager.cpp
+++ Study/workerManager.cpp
@@ -1,6 +1,27 @@
 ﻿#include"workerManager.h"
 WorkerManager::WorkerManager() {
-
+	//文件不存在
+	ifstream ifs;
+	ifs.open(FILENAME, ios::in);
+	if (!ifs.is_open()) {
+		cout << "文件不存在" << endl; //测试输出
+		this->m_EmpNum = 0;  //初始化人数
+		this->m_FileIsEmpty = true; //初始化文件为空标志
+		this->m_EmpArray = NULL; //初始化数组
+		ifs.close(); //关闭文件
+		return;
+	}
+	//文件存在，无数据
+	char ch;
+	ifs >> ch;
+	if (ifs.eof()) {
+		cout << "文件为空" << endl; //测试输出
+		this->m_EmpNum = 0;  //初始化人数
+		this->m_EmpArray = NULL; //初始化数组
+		this->m_FileIsEmpty = true; //初始化文件为空标志
+		ifs.close(); //关闭文件
+		return;
+	}
 }
 void WorkerManager::Show_menu() {
 	cout << "********************************************" << endl;
@@ -22,5 +43,83 @@ void WorkerManager::ExitSystem() {
 	exit(0);
 }
 WorkerManager::~WorkerManager() {
-
+	if (this->m_EmpArray != NULL) {
+		delete[] this->m_EmpArray;
+		this->m_EmpArray = NULL;
+	}
+}
+void WorkerManager::Add_Emp() {
+	cout << "请输入 添加职工数量：" << endl;
+	int addNum = 0;
+	cin >> addNum;
+	if (addNum > 0) {
+		//添加;计算添加新空间大小
+		int newSize = this->m_EmpNum + addNum;
+		//开辟新空间
+		Worker** newSpace = new Worker * [newSize];
+		//将原来空间下的数据拷贝到新空间
+		if (this->m_EmpArray != NULL) {
+			for (int i = 0; i < this->m_EmpNum; i++) {
+				newSpace[i] = this->m_EmpArray[i];
+			}
+		}
+		//批量添加新数据
+		for (int i = 0; i < addNum; i++) {
+			int id;
+			string name;
+			int dSelect;
+			cout << "请输入第" << i + 1 << "个员工的编号:" << endl;
+			cin >> id;
+			cout << "请输入第" << i + 1 << "个员工的姓名:" << endl;
+			cin >> name;
+			cout << "请选择该职工岗位：" << endl;
+			cout << "1、普通职工" << endl;
+			cout << "2、经理" << endl;
+			cout << "3、老板" << endl;
+			cin >> dSelect;
+			Worker* worker = NULL;
+			switch (dSelect) {
+			case 1:
+				worker = new Employee(id, name, 1);
+				break;
+			case 2:
+				worker = new Manager(id, name, 2);
+				break;
+			case 3:
+				worker = new Boss(id, name, 3);
+				break;
+			default:
+				break;
+			}
+			//将创建职工职责，保存到数组中
+			newSpace[this->m_EmpNum + i] = worker;
+		}
+		//释放原有空间
+		delete[] this->m_EmpArray;
+		//更改新空间的指向
+		this->m_EmpArray = newSpace;
+		//更新新的职工人数
+		this->m_EmpNum = newSize;
+		this->m_FileIsEmpty = false;
+		//提示添加成功
+		cout << "成功添加" << addNum << "名新职工" << endl;
+		this->save();
+	}
+	else {
+		cout << "输入有误" << endl;
+	}
+	//按任意键后，清屏回到上级目录
+	system("pause");
+	system("cls");
+}
+void WorkerManager::save() {
+	ofstream ofs;
+	ofs.open(FILENAME, ios::out);//用输出的方式打开文件
+	//将每个人数据写入文件
+	for (int i = 0; i < this->m_EmpNum; i++) {
+		ofs << this->m_EmpArray[i]->m_Id << " "
+			<< this->m_EmpArray[i]->m_Name << " "
+			<< this->m_EmpArray[i]->m_DeptId << endl;
+	}
+	ofs.close();
 }
--- Study/workerManager.h
+++ Study/workerManager.h
@@ -1,10 +1,21 @@
 ﻿#pragma once//防止头文件重复包含
 #include<iostream>
+#include<fstream>
+#include"worker.h"
+#include"boss.h"
+#include"employee.h"
+#include"manager.h"
+#define FILENAME "empFile.txt"
 using namespace std;
 class WorkerManager {
 public:
 	WorkerManager();
 	void Show_menu();
 	void ExitSystem();
+	int m_EmpNum;
+	Worker** m_EmpArray;
+	void Add_Emp();
+	void save();
+	bool m_FileIsEmpty;
 	~WorkerManager();
 };
