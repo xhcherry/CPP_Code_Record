f44806a7e2d56201f7ee24c5b2255a410a1d426b
xhaocherry <cyhcherry@outlook.com>
2023-01-10 01:29:25 +0800

    create

---
+++ Study/string子串.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+void test1() {
+	string str = "asdfgh";
+	string subStr = str.substr(1, 3);
+	cout << subStr << endl;
+}
+void test2() {
+	string email = "lisi@gmail.com";
+	int pos = email.find("@");
+	string name = email.substr(0, pos);
+	cout << name << endl;
+}
+void main() {
+	test1();
+	test2();
+}
