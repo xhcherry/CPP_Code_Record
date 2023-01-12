6a5abd6e75c429fbdc64aefc0f5708e23b46f186
xhaocherry <cyhcherry@outlook.com>
2023-01-13 00:00:50 +0800

    1

---
+++ Study/内建函数_算数仿函数.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+#include<functional>
+//negate一元仿函数 取反仿函数
+void test1() {
+	negate<int>n;
+	cout << n(50) << endl;
+}
+void test2() {
+	plus<int>p;
+	cout << p(10, 20) << endl;
+}
+int main() {
+	test1();
+	test2();
+}
---
+++ Study/函数对象仿函数.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+class Add {
+public:
+	int operator()(int v1,int v2) {
+		return v1 + v2;
+	}
+};
+void test1() {
+	Add add;
+	cout << add(10, 10) << endl;
+}
+class Myp {
+public:
+	Myp() {
+		this->count = 0;
+	}
+	void operator()(string test) {
+		cout << test << endl;
+		this->count++;
+	}
+	int count;
+};
+void test2() {
+	Myp myp;
+	myp("hello");
+	myp("hello");
+	myp("hello");
+	cout << myp.count << endl;
+}
+void doP(Myp& mp, string test) {
+	mp(test);
+}
+void test3() {
+	Myp myp;
+	doP(myp, "he");
+}
+int main() {
+	test1();
+	test2();
+	test3();
+}
