5ee0c1216bf631e994384c8b5277982509225e21
xhaocherry <cyhcherry@outlook.com>
2023-01-13 22:45:22 +0800

    1

---
+++ Study/for_each.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+using namespace std;
+void print1(int val) {
+	cout << val << " ";
+}
+class print2 {
+public:
+	void operator()(int val) {
+		cout << val << " ";
+	}
+};
+void test1() {
+	vector<int>v;
+	for (int i = 0; i < 10; i++) {
+		v.push_back(i);
+	}
+	for_each(v.begin(), v.end(), print1);
+	cout << endl;
+	for_each(v.begin(), v.end(), print2());
+}
+int main() {
+	test1();
+}
---
+++ Study/transform.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Transform {
+public:
+	int operator()(int v) {
+		return v + 10;
+	}
+};
+class print {
+public:
+	void operator()(int val) {
+		cout << val << " ";
+	}
+};
+void test1() {
+	vector<int>v;
+	for (int i = 0; i < 10; i++) {
+		v.push_back(i);
+	}
+	vector<int>vT;//目标容器
+	vT.resize(v.size());//需要提前开辟空间
+	transform(v.begin(), v.end(), vT. begin(), Transform());
+	for_each(vT.begin(), vT.end(),print());
+}
+int main() {
+	test1();
+}
