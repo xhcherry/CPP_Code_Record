34748f9f731046cf854350dbbdae99c7cbdaa12a
xhcherry <cyhcherry@outlook.com>
2023-02-22 22:13:26 +0800

    Update 7_41.cpp

--- Primer/7_41.cpp
+++ Primer/7_41.cpp
@@ -14,6 +14,19 @@ public:
             discount = saleprice / sellingprice;
         cout << "该构造函数接受书号、销售量、原价、实际售价四个信息" << endl;
     }
+    Sales_data() : Sales_data("", 0, 0, 0)
+    {
+        cout << "该构造函数无须接受任何信息" << endl;
+    }
+    Sales_data(const string &book) : Sales_data(book, 0, 0, 0)
+    {
+        cout << "该构造函数接受书号信息" << endl;
+    }
+    Sales_data(std::istream &is) : Sales_data()
+    {
+        read(is, *this);
+        cout << "该构造函数接受用户输入的信息" << endl;
+    }
 
 private:
     string bookNo;
@@ -22,3 +35,24 @@ private:
     double saleprice = 0.0;
     double discount = 0.0;
 };
+std::istream &read(std::istream &is, Sales_data &item)
+{
+    is >> item.bookNo >> item.units_sold >> item.sellingprice >> item.saleprice;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item)
+{
+    os << item.bookNo << " " << item.units_sold << " " << item.sellingprice
+       << " " << item.saleprice << " " << item.discount;
+    return os;
+}
+
+int main()
+{
+    Sales_data first("978-7-121-15535-2", 85, 128, 109);
+    Sales_data second;
+    Sales_data third("978-7-121-15535-2");
+    Sales_data last(cin);
+    return 0;
+}
