e360797160d1dfffb94ee815acc108f101c06404
xhcherry <cyhcherry@outlook.com>
2023-03-03 22:50:24 +0800

    Create 12_15.cpp

---
+++ Primer/12/12_15.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct destination
+{
+};
+struct connection
+{
+};
+
+connection connect(destination *pd)
+{
+    cout << "打开连接" << endl;
+    return connection();
+}
+
+void disconnect(connection c)
+{
+    cout << "关闭连接";
+}
+
+// 未使用 shared_ptr 的版本
+void f(destination &d)
+{
+    cout << "直接管理 connect" << endl;
+    connection c = connect(&d);
+    // 忘记调用 disconnect 关闭连接
+
+    cout << "f 结束" << endl;
+}
+
+void end_connection(connection *p) { disconnect(*p); }
+
+// 使用 shared_ptr 的版本
+void f1(destination &d)
+{
+    cout << "用 shared_ptr 管理 connect" << endl;
+    connection c = connect(&d);
+
+    shared_ptr<connection> p(&c, [](connection *p)
+                             { disconnect(*p); });
+    cout << "f1 结束" << endl;
+}
+
+int main()
+{
+    destination d;
+    f(d);
+    f1(d);
+    return 0;
+}
