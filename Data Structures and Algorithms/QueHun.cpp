fc3868337035c20d1f2e2e6bd888b7cd6358d624
xhcherry <cyhcherry@outlook.com>
2023-03-22 23:20:20 +0800

    1

--- Data Structures and Algorithms/QueHun.cpp
+++ Data Structures and Algorithms/QueHun.cpp
@@ -16,75 +16,54 @@
 现在，小包从36张牌中抽取了13张牌，他想知道在剩下的23张牌中，再取一张牌，取到哪几种数字牌可以和牌。
 */
 #include<iostream>
+#include<map>
 #include<vector>
-#include<algorithm>
 using namespace std;
-bool ishu(vector<int>num)
-{
-    if (num.empty())
-        return true;
-    int count0 = 0;
-    for(int i=0;i<num.size();++i)
-    {
-        if (num[0] == num[i])
-            ++count0;
-        else
-            break;
+bool isHu(map<int, int> mp, int num){
+    if(num<=0)return true;
+    while(mp[mp.begin()->first]==0)mp.erase(mp.begin());
+    map<int, int>::iterator it = mp.begin();
+    if(num%3!=0 && (it->second)>=2){
+        mp[it->first]-=2;
+        if(isHu(mp, num-2))return true;
+        mp[it->first]+=2;
     }
-    if (num.size() % 3 != 0 && count0 >= 2)
-    {
-        vector<int> newnum(num.begin() + 2, num.end());
-        if (ishu(newnum))
-            return true;
+    if((it->second)>=3){
+        mp[it->first]-=3;
+        if(isHu(mp, num-3))return true;
+        mp[it->first]+=3;
     }
-    if (count0 >= 3)
-    {
-        vector<int> newnum(num.begin() + 3, num.end());
-        if (ishu(newnum))
-            return true;
-    }
-    if(count(num.begin(),num.end(),num[0]+1)>0 && count(num.begin(), num.end(), num[0] + 2)>0)
-    {
-        vector<int> newnum(num.begin() + 1, num.end());
-        newnum.erase(find(newnum.begin(), newnum.end(), num[0] + 1));
-        newnum.erase(find(newnum.begin(), newnum.end(), num[0] + 2));
-        if (ishu(newnum))
-            return true;
+    if((it->second)>0 && mp[(it->first)+1]>0 && mp[(it->first)+2]>0){
+        mp[it->first]--;
+        mp[(it->first)+1]--;
+        mp[(it->first)+2]--;
+        if(isHu(mp, num-3))return true;
+        mp[(it->first)]++;
+        mp[(it->first)+1]++;
+        mp[(it->first)+2]++;
     }
     return false;
 }
-bool hupai(vector<int>num, int n)
-{
-    if (count(num.begin(), num.end(), n) == 4)
-        return false;
-    num.push_back(n);
-    sort(num.begin(),num.end());
-    return ishu(num);
-}
-int main()
-{
-    vector<int> num;
-    vector<int> res;
-    for (int i = 0; i < 13; ++i)
-    {
-        int tmp;
-        cin >> tmp;
-        if (tmp > 0 && tmp < 10)
-            num.push_back(tmp);
-        else
-        {
-            cout << "输入错误" << endl;
-            return 0;
+int main(){
+    map<int, int> mp;
+    int tmp;
+    for(int i=0;i<13;i++){
+        cin>>tmp;
+        mp[tmp]++;
+    }
+    vector<int> ans;
+    for(int i=1;i<10;i++){
+        if(mp[i]<4){
+            ++mp[i];
+            if(isHu(mp, 14))
+                ans.push_back(i);
+            --mp[i];
         }
     }
-    for (int n = 1; n < 10; ++n)
-    {
-        if (hupai(num, n))
-            res.push_back(n);
+    if(ans.empty())cout<<0<<endl;
+    else{
+        for(int i:ans)cout<<i<<' ';
     }
-    if (res.empty())
-        cout << 0;
-    else
-        for (int i = 0; i < res.size(); ++i)
-            cout << res[i]<<" ";
+    
+    return 0;
 }
