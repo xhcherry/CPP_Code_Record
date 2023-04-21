14054199baba10cec366999c1e0b5e4e21be7dd3
xhcherry <cyhcherry@outlook.com>
2023-04-22 00:15:28 +0800

    Update inorderTraversal.cpp

--- DSA/leetcode/inorderTraversal.cpp
+++ DSA/leetcode/inorderTraversal.cpp
@@ -18,25 +18,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        if(!root) {
-            return {};
-        }
-        vector<int> res;
-        stack<TreeNode*> sk;
-        TreeNode *cur = root;
-        while(cur || !sk.empty()){
-            if(cur){
-                sk.push(cur);
-                cur = cur->left;
+        vector<int> result;
+        stack<TreeNode*> st;
+        if (root != NULL) st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            if (node != NULL) {
+                st.pop(); // 将该节点弹出，避免重复操作，下面再将右中左节点添加到栈中
+                if (node->right) st.push(node->right);  // 添加右节点（空节点不入栈）
+
+                st.push(node);                          // 添加中节点
+                st.push(NULL); // 中节点访问过，但是还没有处理，加入空节点做为标记。
+
+                if (node->left) st.push(node->left);    // 添加左节点（空节点不入栈）
+            } else { // 只有遇到空节点的时候，才将下一个节点放进结果集
+                st.pop();           // 将空节点弹出
+                node = st.top();    // 重新取出栈中元素
+                st.pop();
+                result.push_back(node->val); // 加入到结果集
             }
-            else{
-                cur = sk.top();
-                sk.pop();
-                res.push_back(cur->val);
-                cur = cur->right;
-            }
         }
-        return res;
+        return result;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
